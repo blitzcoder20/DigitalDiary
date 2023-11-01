f26a87f479278bb50ff4c4cfe1834a9c70ae95ff
Raffaele <Raffaele@Pc-Cameretta.fritz.box>
2023-11-01 17:20:52 +0100

    Initial commit

--- src/main/java/it/begear/digitaldiary/DiarioDigitaleApplication.java
+++ src/main/java/it/begear/digitaldiary/DiarioDigitaleApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package it.begear.digitaldiary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
