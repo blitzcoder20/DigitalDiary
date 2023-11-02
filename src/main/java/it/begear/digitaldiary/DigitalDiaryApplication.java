9de1813dd79b7460718b4dc0258dd5b8ab50540c
Raffaele <Raffaele@Pc-Cameretta.fritz.box>
2023-11-02 16:19:38 +0100

    Project start

--- src/main/java/it/begear/digitaldiary/DigitalDiaryApplication.java
+++ src/main/java/it/begear/digitaldiary/DigitalDiaryApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiarioDigitaleApplication {
+public class DigitalDiaryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiarioDigitaleApplication.class, args);
+		SpringApplication.run(DigitalDiaryApplication.class, args);
 	}
 
 }
